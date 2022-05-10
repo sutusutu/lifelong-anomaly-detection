70e1ff1ae303df6d8f93d7db3243f989fea349a2
sutusutu <66063041+sutusutu@users.noreply.github.com>
2022-05-10 21:17:57 +0800

    Add files via upload

--- model/model.py
+++ model/model.py
@@ -35,52 +35,13 @@ def freeze_parameters(model, backbone, train_fc=False):
             p.requires_grad = False
 
 
-class projector(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super(projector, self).__init__()
-        self.layer1 = nn.Linear(in_dim, in_dim * 2)
-        self.re1 = nn.ReLU()
-        self.bn1 = nn.BatchNorm1d(in_dim * 2)
-        self.layer2 = nn.Linear(in_dim * 2, in_dim)
-        self.re2 = nn.ReLU()
-        self.layer3 = nn.Linear(in_dim, out_dim)
-        self.re3 = nn.ReLU()
-        self.layer4 = nn.Linear(out_dim, out_dim * 2)
-        self.re4 = nn.ReLU()
-        self.bn2 = nn.BatchNorm1d(out_dim * 2)
-        self.layer5 = nn.Linear(out_dim * 2, out_dim)
-
-    def forward(self, x):
-        z1 = self.layer1(x)
-        z1 = self.re1(z1)
-        z1 = self.bn1(z1)
-        z1 = self.layer2(z1)
-        z1 = self.re2(z1)
-        x = x + z1
-        x = self.layer3(x)
-        x = self.re3(x)
-        z2 = self.layer4(x)
-        z2 = self.re4(z2)
-        z2 = self.bn2(z2)
-        z2 = self.layer5(z2)
-        x = x + z2
-
-        return x
-
-
 class init_model(nn.Module):
     def __init__(self):
         super(init_model, self).__init__()
-        # self.bockbone = models.resnet50(pretrained=True)
-        #
-        # self.bockbone.fc = nn.Identity()
-        # freeze_parameters(self.bockbone, 50, train_fc=False)
 
         self.vit = ViT('B_16_imagenet1k', pretrained=True, image_size=224, num_classes=2048)
         self.vit.norm = nn.Identity()
         self.vit.fc = nn.Identity()
-
-        # self.fc = MLP(2048, 1024)
         self.fc = MLP(768, 512)
 
     def forward(self, x):
@@ -101,15 +62,7 @@ class continual_model(nn.Module):
         self.cur_model = deepcopy(pre_model)
         for p in self.pre_model.parameters():
             p.requires_grad = False
-        # self.cur_model = cur_model
 
-        # self.projector = projector(1024, 1024)
-        # self.distill_predictor = nn.Sequential(
-        #     nn.Linear(1024, 512),
-        #     nn.BatchNorm1d(512),
-        #     nn.ReLU(),
-        #     nn.Linear(512, 1024),
-        # )
 
         self.distill_predictor = nn.Sequential(
             nn.Linear(512, 256),
@@ -125,11 +78,8 @@ class continual_model(nn.Module):
 
     def forward(self, x, dis=False):
         pre_fea, pre_z = self.frozen_pre(x)
-        # pre_z = self.projector(pre_z)
-
         cur_fea, cur_z = self.cur_model(x)
 
-        # cur_k = self.projector(cur_z)
         cur_k = self.distill_predictor(cur_z)
         cur_k = F.normalize(cur_k, dim=-1)
 
@@ -137,10 +87,6 @@ class continual_model(nn.Module):
             return pre_fea, pre_z, cur_fea, cur_z, cur_k
         else:
             return cur_fea, cur_k
-
-
-
-        # return pre_fea, pre_z, cur_fea, cur_z
 
     def get_cur_model(self):
         return self.cur_model
